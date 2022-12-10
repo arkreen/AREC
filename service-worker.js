3efbfe0c40f3d38c469ca6485fd2fa9f18c74b27
Lu-Derik <117131602+Lu-Derik@users.noreply.github.com>
2022-12-10 08:10:50 +0000

    Deploying to gh-pages from @ arkreen/ArkRec@4a0b382acfb4bb20de8de3167eb7e972ffbdffb5 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f882429ad86f65679ef3aac1c4afcebd.js"
+  "./precache-manifest.797d93157ba3b82993c7215230cb64a2.js"
 );
 
 self.addEventListener('message', (event) => {
